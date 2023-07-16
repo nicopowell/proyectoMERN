9a63023df5e733852833718489f22b8955c882f6
JulianIgnacio <montenegrojulianig@gmail.com>
2023-07-16 18:56:51 -0300

    maquetando el footer

---
+++ src/Components/common/footer.jsx
@@ -0,0 +1,36 @@
+<footer className="p-3 mb-2 text-white text-center text-lg-start">
+    <section className="row">
+        <aside className="col-md-4 ps-lg-5 col-xs-12">
+
+        </aside>
+        <aside className="col-md-4 text-md-center col-xs-12">
+        <h6>Paginas que puede visitar</h6>
+          <ul>
+            <li>
+              <a href="./pages/acercaDeNosotros.html" className="color-texto fs-6">
+                ¿Quienes Somos?
+              </a>
+            </li>
+            <li>
+              <a href="./pages/error404.html" className="color-texto fs-6">
+                Consultas
+              </a>
+            </li>
+            <li>
+              <a href="./pages/error404.html" className="color-texto fs-6">
+                Categorías
+              </a>
+            </li>
+            <li>
+              <a href="./pages/error404.html" className="color-texto fs-6">
+                Política de Privacidad
+              </a>
+            </li>
+          </ul>
+        </aside>
+        <aside className="col-md-4 text-md-start col-xs-12">
+
+        </aside>
+    </section>
+
+</footer>
