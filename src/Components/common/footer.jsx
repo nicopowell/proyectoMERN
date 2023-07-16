fc86bb19d72d9c819c538313b30b05160f0be2f6
JulianIgnacio <montenegrojulianig@gmail.com>
2023-07-16 19:01:50 -0300

    estructurando footer

--- src/Components/common/footer.jsx
+++ src/Components/common/footer.jsx
@@ -1,36 +1,41 @@
-<footer className="p-3 mb-2 text-white text-center text-lg-start">
-    <section className="row">
-        <aside className="col-md-4 ps-lg-5 col-xs-12">
+const Footer = () => {
 
-        </aside>
-        <aside className="col-md-4 text-md-center col-xs-12">
-        <h6>Paginas que puede visitar</h6>
-          <ul>
-            <li>
-              <a href="./pages/acercaDeNosotros.html" className="color-texto fs-6">
-                ¿Quienes Somos?
-              </a>
-            </li>
-            <li>
-              <a href="./pages/error404.html" className="color-texto fs-6">
-                Consultas
-              </a>
-            </li>
-            <li>
-              <a href="./pages/error404.html" className="color-texto fs-6">
-                Categorías
-              </a>
-            </li>
-            <li>
-              <a href="./pages/error404.html" className="color-texto fs-6">
-                Política de Privacidad
-              </a>
-            </li>
-          </ul>
-        </aside>
-        <aside className="col-md-4 text-md-start col-xs-12">
+    return (
+        <footer className="p-3 mb-2 text-white text-center text-lg-start">
+            <section className="row">
+                <aside className="col-md-4 ps-lg-5 col-xs-12">
 
-        </aside>
-    </section>
+                </aside>
+                <aside className="col-md-4 text-md-center col-xs-12">
+                    <h6>Paginas que puede visitar</h6>
+                        <ul>
+                            <li>
+                                <a href="./pages/acercaDeNosotros.html" className="color-texto fs-6">
+                                    ¿Quienes Somos?
+                                </a>
+                            </li>
+                            <li>
+                                <a href="./pages/error404.html" className="color-texto fs-6">
+                                    Consultas
+                                </a>
+                            </li>
+                            <li>
+                                <a href="./pages/error404.html" className="color-texto fs-6">
+                                    Categorías
+                                </a>
+                            </li>
+                            <li>
+                                <a href="./pages/error404.html" className="color-texto fs-6">
+                                    Política de Privacidad
+                                </a>
+                            </li>
+                        </ul>
+                </aside>
+                <aside className="col-md-4 text-md-start col-xs-12">
 
-</footer>
+                </aside>
+            </section>
+        </footer>
+    );
+};
+export default Footer;
