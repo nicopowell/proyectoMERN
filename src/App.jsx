849e0fac43e1658bcd548c00768da5c2f4cbce7d
JulianIgnacio <montenegrojulianig@gmail.com>
2023-07-16 23:13:20 -0300

    agregando pagina principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import './App.css'
 import 'react-bootstrap'
+import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import PaginaPrincipal from './components/views/PaginaPrincipal'
 import Footer from './Components/common/footer';
 
 function App() {
   return (
+    <>
     <BrowserRouter>
     <Routes>
-      <Route></Route>
+      <Route exact path='/' element={<PaginaPrincipal></PaginaPrincipal>}></Route>
     </Routes>
     <Footer></Footer>
     </BrowserRouter>
+    </>
   )
 }
 
