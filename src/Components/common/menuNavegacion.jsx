b6c1f1ba42f80d1e1701c2818239b783ceef6d94
Maoi11 <marisaindianagarat@gmail.com>
2023-07-20 15:01:48 -0300

    se agregan estilos y logo al navbar

--- src/Components/common/menuNavegacion.jsx
+++ src/Components/common/menuNavegacion.jsx
@@ -1,29 +1,62 @@
-import { Navbar, Container, NavDropdown, Nav } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown, Container, Button } from "react-bootstrap";
 import { Person, Cart } from "react-bootstrap-icons";
+import "./menuNavegacion.css";
 
 const MenuNavegacion = () => {
-    return (
-        <Navbar bg="dark" variant="dark" expand="lg">
-            <Container className="d-flex justify-content-between">
-                <Navbar.Brand href="#home"><img src="../"></img></Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id='basic-navbar-nav'>
-                    <Nav className="ms-auto">
-                        <Nav.Link href='#'>Pagina Principal</Nav.Link>
-                        <NavDropdown title="Nuestros Productos" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#bebidas-sin-alcohol">Bebidas sin alcohol</NavDropdown.Item>
-                            <NavDropdown.Item href="#bebidas-con-alcohol">Bebidas con alcohol</NavDropdown.Item>
-                            <NavDropdown.Item href="#pizzas">Pizzas</NavDropdown.Item>
-                            <NavDropdown.Item href="#pastas">Pastas</NavDropdown.Item>
-                        </NavDropdown>
-                        <Nav.Link href='#'>Administrador</Nav.Link>
-                        <Nav.Link href='#'>Login</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-            
-        </Navbar>
-    );
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg" id="menuNavbar">
+      <Container className="d-flex justify-content-between">
+        <Navbar.Brand href="#home">
+          <img
+            src="../../src/assets/LogoConLemaNaranja.png"
+            className="logoNavbar p-3"
+          ></img>
+        </Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto align-items-center">
+            <Nav.Link href="#">Pagina Principal</Nav.Link>
+            <NavDropdown title="Nuestros Productos" className="dropMenu">
+              <NavDropdown.Item href="#pizzas">Pizzas</NavDropdown.Item>
+              <NavDropdown.Item href="#pastas">Pastas</NavDropdown.Item>
+              <NavDropdown.Item href="#bebidas-sin-alcohol">
+                Bebidas sin alcohol
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#bebidas-con-alcohol">
+                Bebidas con alcohol
+              </NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Administrador" className="dropMenu">
+              <NavDropdown.Item href="#usuarios">Usuarios</NavDropdown.Item>
+              <NavDropdown.Item href="#productos">Productos</NavDropdown.Item>
+            </NavDropdown>
+
+            <NavDropdown title="Login" className="dropMenu">
+              <NavDropdown.Item>
+                <Person></Person> Iniciar Sesión
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <div className="dropdown-header">
+                ¿No tienes cuenta? Regístrate.
+              </div>
+              <NavDropdown.Item className="text-center">
+                <Button className="btnRegistro btn">Registro</Button>
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link
+              href="#carrito"
+              id="carrito"
+              className="d-flex flex-column align-items-center"
+            >
+              <Cart size={35}></Cart>
+              {/* <p className="my-1">Tu carrito</p> */}
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default MenuNavegacion;
