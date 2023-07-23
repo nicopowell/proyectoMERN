1fc8d023d31075be79689138892823dba15cf0cc
Nicolas Powell <nicopowell30@gmail.com>
2023-07-23 01:58:02 -0300

    cree el encabezado de la tabla de pedidos

--- src/components/views/administrador/TablaPedidos.jsx
+++ src/components/views/administrador/TablaPedidos.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Table } from 'react-bootstrap';
+import ItemPedido from './ItemPedido';
 
 const TablaPedidos = () => {
     return (
-        <div>
-            
-        </div>
+        <Table responsive striped bordered hover>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Usuario</th>
+                    <th>Fecha</th>
+                    <th>Productos</th>
+                    <th>Estado</th>
+                    <th>Opciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <ItemPedido></ItemPedido>
+            </tbody>
+        </Table>
     );
 };
 
