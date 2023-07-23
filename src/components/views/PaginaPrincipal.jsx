5fecd0cfb0b59a9a6ac41a2f804c9d8e7c7be0ca
Nicolas Powell <nicopowell30@gmail.com>
2023-07-23 02:28:09 -0300

    puse el logo en el index

--- src/components/views/PaginaPrincipal.jsx
+++ src/components/views/PaginaPrincipal.jsx
@@ -1,4 +1,3 @@
-
 import { Container } from "react-bootstrap";
 import ProductosPopulares from "./productos/ProductosPopulares";
 import GrillaProductos from "./productos/GrillaProductos";
@@ -14,8 +13,16 @@ const PaginaPrincipal = () => {
                 id="bannerIndex"
             />
             <section className="seccionInfoRestaurante text-light py-3 text-center mb-5">
-                <h1>Nombre del restaurante</h1>
-                <p>Informacion extra</p>
+                <img
+                    src="../../src/assets/LogoConLemaNaranja.png"
+                    alt="Logo del restaurante Rolling Bites"
+                    id="logoIndex"
+                />
+                <p className="lead fw-semibold px-5 my-4">
+                    ¡Bienvenido a Rolling Bites! Descubre sabores exquisitos con productos
+                    frescos y de calidad superior. Nuestro equipo de chefs apasionados te espera
+                    para brindarte una experiencia gastronómica inolvidable. ¡Ven y disfruta!
+                </p>
             </section>
             <Container className="mb-5">
                 <h3>Productos populares</h3>
