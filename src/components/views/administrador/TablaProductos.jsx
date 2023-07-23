f4fba9d47af65fc74d1daa9d96e39d6a39fe9b98
Nicolas Powell <nicopowell30@gmail.com>
2023-07-23 01:46:05 -0300

    cree el encabezado de la tabla de Productos

--- src/components/views/administrador/TablaProductos.jsx
+++ src/components/views/administrador/TablaProductos.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import { Table } from 'react-bootstrap';
 import ItemUsuario from './ItemUsuario';
+import ItemProducto from './ItemProducto';
 
 const TablaProductos = () => {
     return (
-        <></>
+        <Table responsive striped bordered hover>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Producto</th>
+                    <th>Detalle</th>
+                    <th>Categoria</th>
+                    <th>Precio</th>
+                    <th>URL Imagen</th>
+                    <th>Estado</th>
+                    <th>Opciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <ItemProducto></ItemProducto>
+                <ItemProducto></ItemProducto>
+            </tbody>
+        </Table>
     );
 };
 
