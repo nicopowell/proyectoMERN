1a131a857230adea2afaf67fb25715877b1938c0
Nicolas Powell <nicopowell30@gmail.com>
2023-07-23 01:54:19 -0300

    agregué el componente ItemProducto

--- src/components/views/administrador/ItemProducto.jsx
+++ src/components/views/administrador/ItemProducto.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React from "react";
+import { Button } from "react-bootstrap";
+import "./administrador.css";
 
 const ItemProducto = () => {
     return (
-        <div>
-            
-        </div>
+        <tr>
+            <td>1</td>
+            <td>Pizza</td>
+            <td>Pizza de Masa madre</td>
+            <td>Pizzas</td>
+            <td>$1.600</td>
+            <td className="columnaUrl">
+                https://images.pexels.com/photos/3682837/pexels-photo-3682837.jpeg
+            </td>
+            <td>Activo</td>
+            <td className="text-center">
+                <Button variant="warning my-1 mx-1">Editar</Button>
+                <Button variant="danger">Borrar</Button>
+            </td>
+        </tr>
     );
 };
 
