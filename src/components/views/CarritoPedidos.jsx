b681219486f229f5ab0da75bdaefcd04853d5750
Maoi11 <marisaindianagarat@gmail.com>
2023-07-23 22:04:54 -0300

    se agrega tabla, sus estilos y configuraciones

--- src/components/views/CarritoPedidos.jsx
+++ src/components/views/CarritoPedidos.jsx
@@ -1,13 +1,36 @@
 import './carrito/estilosCarrito.css';
 import ItemCarrito from './carrito/ItemCarrito';
-import { Container, Col, Row, ListGroup, Button} from 'react-bootstrap'
+import { Container, Col, Row, ListGroup, Button, Table} from 'react-bootstrap'
 import { Trash3Fill } from 'react-bootstrap-icons';
 
 const CarritoPedidos = () => {
     return (
         <>
         <section>
-<h1>HOLA MUNDO</h1>
+            <Container>
+            <h1 className='mt-5 p-2 titulo' >Tu pedido</h1>
+            <Row>
+                <Col lg={8}>
+                    <Table striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th>Cantidad</th>
+                                <th>Producto</th>
+                                <th>Precio</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <ItemCarrito></ItemCarrito>
+                            <ItemCarrito></ItemCarrito>
+                            <ItemCarrito></ItemCarrito>
+                        </tbody>
+
+                    </Table>
+                </Col>
+            </Row>
+            </Container>
+
         </section>
 
             
--- src/components/views/carrito/ItemCarrito.jsx
+++ src/components/views/carrito/ItemCarrito.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { useState } from 'react';
+import { Trash2 } from 'react-bootstrap-icons';
 
 const ItemCarrito = () => {
     return (
         <>
-            
+            <tr>
+<td>1</td>
+<td>Pizza</td>
+<td>$2500</td>
+<td><Trash2></Trash2></td>
+
+            </tr>
         </>
     );
 };
