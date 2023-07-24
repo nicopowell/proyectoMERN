d25e6461f58a30a04316fb0162c5b76f782ce322
Maoi11 <marisaindianagarat@gmail.com>
2023-07-23 21:29:03 -0300

    se corrigen rutas y agrega estructura faltante para maquetar carrito

--- src/components/views/CarritoPedidos.jsx
+++ src/components/views/CarritoPedidos.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import './carrito/estilosCarrito.css';
+import ItemCarrito from './carrito/ItemCarrito';
+import { Container, Col, Row, ListGroup, Button} from 'react-bootstrap'
+import { Trash3Fill } from 'react-bootstrap-icons';
 
 const CarritoPedidos = () => {
     return (
-        <div>
+        <>
+        <section>
+<h1>HOLA MUNDO</h1>
+        </section>
+
             
-        </div>
+        </>
     );
 };
 
