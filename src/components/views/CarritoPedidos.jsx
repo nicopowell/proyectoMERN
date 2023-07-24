3a14df43caa28c03d2944e2d4baad2aa849a88b5
Maoi11 <marisaindianagarat@gmail.com>
2023-07-23 22:46:06 -0300

    estilos titulo e imagenes, centrado de columnas y botones

--- src/components/views/CarritoPedidos.jsx
+++ src/components/views/CarritoPedidos.jsx
@@ -1,49 +1,54 @@
-import './carrito/estilosCarrito.css';
-import ItemCarrito from './carrito/ItemCarrito';
-import { Container, Col, Row, ListGroup, Button, Table} from 'react-bootstrap'
-import { Trash3Fill } from 'react-bootstrap-icons';
-
+import "./carrito/estilosCarrito.css";
+import ItemCarrito from "./carrito/ItemCarrito";
+import { Container, Col, Row, ListGroup, Button, Table } from "react-bootstrap";
+import { Trash3Fill } from "react-bootstrap-icons";
 
 const CarritoPedidos = () => {
-    return (
-        <>
-        <section>
-            <Container>
-            <h1 className='mt-5 p-2 titulo' >Tu pedido</h1>
-            <Row>
-                <Col lg={8}>
-                    <Table striped bordered hover>
-                        <thead>
-                            <tr>
-                                <th>Cantidad</th>
-                                <th>Producto</th>
-                                <th>Precio</th>
-                                <th></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <ItemCarrito></ItemCarrito>
-                            <ItemCarrito></ItemCarrito>
-                            <ItemCarrito></ItemCarrito>
-                        </tbody>
-
-                    </Table>
-                </Col>
-                <Col lg={4}>
-                    <aside>
-                        <img src='../../src/assets/18.png' alt="logoRB" className='sombra' />
-                        <p className='display-6'>Total a pagar: ${3000}</p>
-                        <Button>Confirmar pedido</Button>
-                    </aside>
-                </Col>
-            </Row>
-            </Container>
-
-        </section>
-
-            
-        </>
-    );
+  return (
+    <>
+      <section className="my-4">
+        <Container className=" align-items-center ">
+          <h1 className="mt-5 p-2 titulo">Tu pedido</h1>
+          <Row>
+            <Col lg={8}>
+              <Table hover>
+                <thead>
+                  <tr>
+                    <th>Cantidad</th>
+                    <th>Producto</th>
+                    <th>Precio</th>
+                    <th></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <ItemCarrito></ItemCarrito>
+                  <ItemCarrito></ItemCarrito>
+                  <ItemCarrito></ItemCarrito>
+                </tbody>
+              </Table>
+              <div className="d-flex justify-content-center">
+              <Button id="btnSeguirComprando" className>Seguir comprando</Button>
+              </div>
+            </Col>
+            <Col lg={4}>
+              <aside>
+                <Container  className="d-flex justify-content-center align-items-center flex-column">
+                  <img
+                    src="../../src/assets/18.png"
+                    alt="logoRB"
+                    className="sombra"
+                  />
+                  <h5>Total a pagar: ${3000}</h5>{" "}
+                  {/*enviar state suma de productos*/}
+                  <Button id="btnConfirmarPedido">Confirmar pedido</Button>
+                </Container>
+              </aside>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </>
+  );
 };
 
 export default CarritoPedidos;
--- src/components/views/carrito/ItemCarrito.jsx
+++ src/components/views/carrito/ItemCarrito.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import { Trash3 } from 'react-bootstrap-icons';
+import React from "react";
+import { Trash3 } from "react-bootstrap-icons";
 
 const ItemCarrito = () => {
-    return (
-        <>
-            <tr>
-<td>1</td>
-<td>Pizza</td>
-<td>2500</td>
-<td><Trash3></Trash3></td>
-
-            </tr>
-        </>
-    );
+  return (
+    <>
+      <tr>
+        <td>1</td>
+        <td>Pizza</td>
+        <td>2500</td>
+        <td>
+          <Trash3></Trash3>
+        </td>
+      </tr>
+    </>
+  );
 };
 
 export default ItemCarrito;
