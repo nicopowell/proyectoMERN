97328f5af17feae5f795fac3679932bc4d90d97d
Maoi11 <marisaindianagarat@gmail.com>
2023-07-23 21:16:43 -0300

    se agrega archivos de carrito e item carrito

---
+++ src/components/views/CarritoPedidos.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CarritoPedidos = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default CarritoPedidos;
---
+++ src/components/views/carrito/ItemCarrito.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useState } from 'react';
+
+const ItemCarrito = () => {
+    return (
+        <>
+            
+        </>
+    );
+};
+
+export default ItemCarrito;
