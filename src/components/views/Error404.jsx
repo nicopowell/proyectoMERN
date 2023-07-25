7d88408c337af743e5db71433bf84992861c2d99
JulianIgnacio <104681273+JulianIgnacio@users.noreply.github.com>
2023-07-24 22:46:24 -0300

    Update Error404.jsx

--- src/components/views/Error404.jsx
+++ src/components/views/Error404.jsx
@@ -6,7 +6,7 @@ const Error404 = () => {
   return (
     <>
       <Container className="d-flex flex-column align-items-center justify-content-center">
-        <h1 className="my-5 titulo">¡Eso aún se está cocinando!</h1>
+        <h1 className="my-5 titulo404">¡Eso aún se está cocinando!</h1>
         
         <img src="../../src/assets/404.gif" alt="error404" className="imgError align-items-center img-fluid"></img>
         <Link to="/" className="mb-5 titulo404 fs-3" id="volverAInicio">
