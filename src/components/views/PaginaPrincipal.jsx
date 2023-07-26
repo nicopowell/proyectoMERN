400b97b9ae7d3c385fbe45a8570bb59e6af0c30c
Maoi11 <marisaindianagarat@gmail.com>
2023-07-26 00:23:51 -0300

    se agrega funcionalidad con props y state entre pagina principal, grillaproductos y card productos

--- src/components/views/PaginaPrincipal.jsx
+++ src/components/views/PaginaPrincipal.jsx
@@ -5,44 +5,45 @@ import InputProductos from "./productos/InputProductos";
 import { useState } from "react";
 
 const PaginaPrincipal = () => {
-    const [carrito, setCarrito] = useState([]);
-    const agregarAlCarrito = (producto) => {
-        setCarrito([...carrito, producto]);
-    }
+  const [carrito, setCarrito] = useState([]);
+  const onAgregarAlCarrito = (producto) => {
+    setCarrito([...carrito, producto]);
+  };
 
-    return (
-        <div className="mainSection">
-            <img
-                src="https://images.pexels.com/photos/8753672/pexels-photo-8753672.jpeg"
-                alt="Banner con platos de comida"
-                className="w-100"
-                id="bannerIndex"
-            />
-            <section className="seccionInfoRestaurante text-light py-3 text-center mb-5">
-                <img
-                    src="../../src/assets/LogoConLemaNaranja.png"
-                    alt="Logo del restaurante Rolling Bites"
-                    id="logoIndex"
-                />
-                <p className="lead fw-semibold px-5 my-4">
-                    ¡Bienvenido a Rolling Bites! Descubre sabores exquisitos con productos
-                    frescos y de calidad superior. Nuestro equipo de chefs apasionados te espera
-                    para brindarte una experiencia gastronómica inolvidable. ¡Ven y disfruta!
-                </p>
-            </section>
-            <Container className="mb-5">
-                <h3>Productos populares</h3>
-                <Container>
-                    <ProductosPopulares></ProductosPopulares>
-                </Container>
-                <h3 className="mt-5">Todos nuestros productos</h3>
-                <Container>
-                    <InputProductos></InputProductos>
-                    <GrillaProductos></GrillaProductos>
-                </Container>
-            </Container>
-        </div>
-    );
+  return (
+    <div className="mainSection">
+      <img
+        src="https://images.pexels.com/photos/8753672/pexels-photo-8753672.jpeg"
+        alt="Banner con platos de comida"
+        className="w-100"
+        id="bannerIndex"
+      />
+      <section className="seccionInfoRestaurante text-light py-3 text-center mb-5">
+        <img
+          src="../../src/assets/LogoConLemaNaranja.png"
+          alt="Logo del restaurante Rolling Bites"
+          id="logoIndex"
+        />
+        <p className="lead fw-semibold px-5 my-4">
+          ¡Bienvenido a Rolling Bites! Descubre sabores exquisitos con productos
+          frescos y de calidad superior. Nuestro equipo de chefs apasionados te
+          espera para brindarte una experiencia gastronómica inolvidable. ¡Ven y
+          disfruta!
+        </p>
+      </section>
+      <Container className="mb-5">
+        <h3>Productos populares</h3>
+        <Container>
+          <ProductosPopulares></ProductosPopulares>
+        </Container>
+        <h3 className="mt-5">Todos nuestros productos</h3>
+        <Container>
+          <InputProductos></InputProductos>
+          <GrillaProductos onAgregarAlCarrito={onAgregarAlCarrito}></GrillaProductos>
+        </Container>
+      </Container>
+    </div>
+  );
 };
 
 export default PaginaPrincipal;
--- src/components/views/productos/GrillaProductos.jsx
+++ src/components/views/productos/GrillaProductos.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { Row } from 'react-bootstrap';
 import CardProducto from './CardProducto';
 
-const GrillaProductos = () => {
+const GrillaProductos = ({onAgregarAlCarrito}) => {
+    /* Aqui al agregar el read, hay que poner un prop que diga: onAgregarAlCarrito={onAgregarAlCarrito} */
     return (
         <Row>
             <CardProducto></CardProducto>    
