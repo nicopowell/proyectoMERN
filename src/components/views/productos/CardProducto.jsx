8526c90fddb8622bb6c958af2b04a8308c9f24d9
Maoi11 <marisaindianagarat@gmail.com>
2023-07-31 13:29:57 -0300

    se agrega sweet alert para indicar que se agrega producto al carrito

--- src/components/views/productos/CardProducto.jsx
+++ src/components/views/productos/CardProducto.jsx
@@ -1,55 +1,68 @@
-import React from "react";
 import { Button, Card, Col } from "react-bootstrap";
 import { CartFill } from "react-bootstrap-icons";
 import ModalDetalleProducto from "../ModalDetalleProducto";
 import { useState } from "react";
-import './cardProducto.css'
+import "./cardProducto.css";
+import Swal from "sweetalert2";
 
-const CardProducto = ({producto, onAgregarAlCarrito}) => {
+const CardProducto = ({ producto, onAgregarAlCarrito }) => {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
   if (!producto) {
     return null;
   }
-  const agregarAlCarrito = ()=> {
+  const agregarAlCarrito = () => {
     onAgregarAlCarrito(producto);
-  }
-  return (
-    <>
-      <Col md={6} lg={3} className="my-2">
-      <Card className="cardProducto">
-                <div className="position-relative">
-                    <Card.Img
-                        variant="top"
-                        src={producto.imagen}
-                        alt={producto.nombre}
-                        className="position-relative imagenProductoCard"
-                    />
-                    <Button className="btnCard btnAgregarAlPedido" onClick={agregarAlCarrito}>
-                        <CartFill size={23}></CartFill>
-                    </Button>
-                </div>
+    Swal.fire(
+      "Producto agregado",
+      "Se agregó el producto a tu carrito",
+      "success"
+    );
+  };
 
-                <Card.Body className="d-flex align-items-center justify-content-between">
-                    <div>
-                        <Card.Title>{producto.nombre}</Card.Title>
-                        <Card.Text className="text-secondary">${producto.precio}</Card.Text>
-                    </div>
-            <div>
-              <Button
-                variant="primary btnCard fw-semibold p-3 p-lg-2 text-light"
-                type="submit"
-                onClick={handleShow}
-              >
-                Detalle
-              </Button>
-            </div>
-          </Card.Body>
-        </Card>
-      </Col>
-      <ModalDetalleProducto producto={producto} show={show} handleClose={handleClose} />
-    </>
-  );
-  }
+return (
+  <>
+    <Col md={6} lg={3} className="my-2">
+      <Card className="cardProducto">
+        <div className="position-relative">
+          <Card.Img
+            variant="top"
+            src={producto.imagen}
+            alt={producto.nombre}
+            className="position-relative imagenProductoCard"
+          />
+          <Button
+            className="btnCard btnAgregarAlPedido"
+            onClick={agregarAlCarrito}
+          >
+            <CartFill size={23}></CartFill>
+          </Button>
+        </div>
+
+        <Card.Body className="d-flex align-items-center justify-content-between">
+          <div>
+            <Card.Title>{producto.nombre}</Card.Title>
+            <Card.Text className="text-secondary">${producto.precio}</Card.Text>
+          </div>
+          <div>
+            <Button
+              variant="primary btnCard fw-semibold p-3 p-lg-2 text-light"
+              type="submit"
+              onClick={handleShow}
+            >
+              Detalle
+            </Button>
+          </div>
+        </Card.Body>
+      </Card>
+    </Col>
+    <ModalDetalleProducto
+      producto={producto}
+      show={show}
+      handleClose={handleClose}
+    />
+  </>
+); };
+
 export default CardProducto;
