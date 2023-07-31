bf40180f519f6a764193d9c9f2589cd39a56b881
Maoi11 <marisaindianagarat@gmail.com>
2023-07-31 14:29:25 -0300

    se corrige navegacion

--- src/components/views/CarritoPedidos.jsx
+++ src/components/views/CarritoPedidos.jsx
@@ -2,12 +2,10 @@ import "./carrito/estilosCarrito.css";
 import ItemCarrito from "./carrito/ItemCarrito";
 import { Container, Col, Row, Button, Table } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 import { agregarPedido } from "../helpers/queris";
-
-
 
 const CarritoPedidos = ({ carrito, setCarrito }) => {
   const navegacion = useNavigate();
@@ -25,83 +23,101 @@ const CarritoPedidos = ({ carrito, setCarrito }) => {
       text: "¿Deseas eliminar este producto de tu carrito?",
       icon: "question",
       showCancelButton: true,
-      confirmButtonText: "Eliminar"
+      confirmButtonText: "Eliminar",
     }).then((result) => {
       if (result.isConfirmed) {
-      const carritoActualizado = carrito.filter(
-      (item) => item.producto.id !== productoId
-    );
-    setCarrito(carritoActualizado);  
+        const carritoActualizado = carrito.filter(
+          (item) => item.producto.id !== productoId
+        );
+        setCarrito(carritoActualizado);
       }
-    }
-    
-  )};
+    });
+  };
 
   const confirmarPedido = (carrito, total) => {
-agregarPedido(carrito, total).then((respuestaCreated=> {
-  if (respuestaCreated && respuestaCreated.status === 201){
-    Swal.fire(
-      "Pedido exitoso", "¡Tu pedido fue realizado y pronto llegará a tu mesa!", "success"
-    );
-    setCarrito([]);
-    navegacion("/");
-  } else {
-    Swal.fire("Tuvimos un problema", "No pudimos concretar tu pedido. Intentá de nuevo mas tarde", "error");
-  }
-}))
-  }
+    agregarPedido(carrito, total).then((respuestaCreated) => {
+      if (respuestaCreated && respuestaCreated.status === 201) {
+        Swal.fire(
+          "Pedido exitoso",
+          "¡Tu pedido fue realizado y pronto llegará a tu mesa!",
+          "success"
+        );
+        setCarrito([]);
+        navegacion("/");
+      } else {
+        Swal.fire(
+          "Tuvimos un problema",
+          "No pudimos concretar tu pedido. Intentá de nuevo mas tarde",
+          "error"
+        );
+      }
+    });
+  };
   return (
     <>
       <section className="my-4 mainSection">
         <Container className=" align-items-center ">
           <h1 className="mt-5 p-2 titulo">Tu pedido</h1>
-          {carrito.length === 0 ? ( 
+          {carrito.length === 0 ? (
             <article className="text-center">
-            <h2>Tu carrito está vacío</h2>
-            <Link to="/" className="mb-3 btn btn-light" id="linkAgregarProductos">
+              <h3>Tu carrito está vacío</h3>
+              <Link
+                to="/"
+                className="mb-3 btn btn-light"
+                id="linkAgregarProductos"
+              >
                 Agregar productos
               </Link>
-              </article>
+            </article>
           ) : (
-          <Row>
-            <Col lg={8}>
-              <div className="table-responsive">
-                <Table hover className="table-sm">
-                  <thead>
-                    <tr>
-                      <th>Cantidad</th>
-                      <th>Producto</th>
-                      <th>Precio</th>
-                      <th></th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {carrito.map((producto) => (
-                      <ItemCarrito key={producto.producto.id} item={producto} eliminarProducto={eliminarProducto}/>
-                    ))}
-                  </tbody>
-                </Table>
-              </div>
-            </Col>
-            <Col lg={4}>
-              <aside>
-                <Container className="d-flex justify-content-center align-items-center flex-column">
-                  <img
-                    src="../../src/assets/18.png"
-                    alt="logoRB"
-                    className="logoConSombra"
-                  />
-                  <h5>Total a pagar: ${calcularTotal()}</h5>
-                  <Button id="btnConfirmarPedido" className="mt-2" onClick={()=> confirmarPedido(carrito,calcularTotal())} disabled={carrito.length === 0}>
-                    Confirmar pedido
-                  </Button>
-                  <Link to="/" className="mb-3" id="linkSeguirComprando">
-                    Seguir comprando
-                  </Link>
-                </Container>
-              </aside>
-            </Col>
-          </Row>
+            <Row>
+              <Col lg={8}>
+                <div className="table-responsive">
+                  <Table hover className="table-sm">
+                    <thead>
+                      <tr>
+                        <th>Cantidad</th>
+                        <th>Producto</th>
+                        <th>Precio</th>
+                        <th></th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {carrito.map((producto) => (
+                        <ItemCarrito
+                          key={producto.producto.id}
+                          item={producto}
+                          eliminarProducto={eliminarProducto}
+                        />
+                      ))}
+                    </tbody>
+                  </Table>
+                </div>
+              </Col>
+              <Col lg={4}>
+                <aside>
+                  <Container className="d-flex justify-content-center align-items-center flex-column">
+                    <img
+                      src="../../src/assets/18.png"
+                      alt="logoRB"
+                      className="logoConSombra"
+                    />
+                    <h5>Total a pagar: ${calcularTotal()}</h5>
+                    <Button
+                      id="btnConfirmarPedido"
+                      className="mt-2"
+                      onClick={() => confirmarPedido(carrito, calcularTotal())}
+                      disabled={carrito.length === 0}
+                    >
+                      Confirmar pedido
+                    </Button>
+                    <Link to="/" className="mb-3" id="linkSeguirComprando">
+                      Seguir comprando
+                    </Link>
+                  </Container>
+                </aside>
+              </Col>
+            </Row>
           )}
         </Container>
       </section>
