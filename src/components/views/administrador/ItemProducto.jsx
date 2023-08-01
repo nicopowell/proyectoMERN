58eebaac384c06ed1fbff3b072c4d430f0c64798
Juan Manuel Gimenez <juanmauel20@gmail.com>
2023-07-31 22:50:03 -0300

    borrar producto se definio estructura pero falta que tome la base de datos para borrar

--- src/components/views/administrador/ItemProducto.jsx
+++ src/components/views/administrador/ItemProducto.jsx
@@ -2,10 +2,48 @@ import React from "react";
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./administrador.css";
+import { consultaBorrarProducto, consultaListaProductos } from "../../helpers/queris";
+
 
 const ItemProducto = ({producto, numeroDeProducto}) => {
+    const borrarProducto = () => {
+        Swal.fire({
+          title: `¿Esta seguro de borrar el producto ${producto.nombre}?`,
+          text: "No se puede revertir este paso",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#00afb9",
+          cancelButtonColor: "#d96c06",
+          confirmButtonText: "Borrar",
+          cancelButtonText: "Cancelar",
+        }).then((result) => {
+          if (result.isConfirmed) {
+              consultaBorrarProducto(producto.id).then((respuesta) => {
+              console.log(respuesta);
+              if (respuesta.status === 200) {
+                Swal.fire(
+                  "Producto borrado",
+                  `El ${producto.nombre} fue borrado correctamente`,
+                  "success"
+                );
+                  consultaListaProductos().then((respuesta) =>
+                  setProductos(respuesta)
+                );
+              } else {
+                Swal.fire(
+                  "Hubo un problema",
+                  `Intente realizar esta operación dentro de unos minutos`,
+                  "success"
+                );
+              }
+            });
+          }
+        });
+      };
+
     return (
         <tr>
+            
             <td>{numeroDeProducto}</td>
             <td>{producto.nombre}</td>
             <td>{producto.detalle}</td>
@@ -20,7 +58,9 @@ const ItemProducto = ({producto, numeroDeProducto}) => {
                     Suspender
                 </Button>
                 <Link className="btn btn-warning" to={'/administrador/editarProducto/'+producto.id}>Editar</Link>
-                <Button variant="danger">Borrar</Button>
+                <Button variant="danger" onClick={borrarProducto}>
+          Borrar
+        </Button>
             </td>
         </tr>
     );
