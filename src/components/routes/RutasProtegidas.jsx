cf10eb61cfc3adf7f62ba21c59b9e38fbdf272ad
Nicolas Powell <nicopowell30@gmail.com>
2023-08-02 05:11:58 -0300

    corregí la ruta del administrador

--- src/components/routes/RutasProtegidas.jsx
+++ src/components/routes/RutasProtegidas.jsx
@@ -3,7 +3,8 @@ import { Navigate } from "react-router-dom";
 const RutasProtegidas = ({children}) => {
  const usuarioLogueado = JSON.parse(sessionStorage.getItem('usuario')) || null;
  //preguntar si el usuario NO esta logueado
- if(!usuarioLogueado || usuarioLogueado !== "Admin"){
+ console.log(usuarioLogueado)
+ if(!usuarioLogueado || usuarioLogueado.perfil !== "Admin"){
     return <Navigate to={"/"}></Navigate>
  }else{
     return children;
